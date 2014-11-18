2f543092f51a9e901c0268b92fbcc00887033901
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-18 16:19:51 +0000

    Add validation to check price is zero or greater

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
 	validates :name, :description, :image_url, :price, :availability_date, :category, :active, presence: true
-
+	validates :price, numericality: {greater_than_or_equal_to: 0.00}
 end
