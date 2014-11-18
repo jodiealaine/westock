b0387f840bbe87689ff703e68ebe1d803685bc81
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-18 16:49:29 +0000

    Add seed data for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create!(name: 'St Ives Summer Pack',
+	description: 
+		%{<p>Gorgeous set of photographs taken during the summer 
+			of 2014 in St Ives, Cornwall.
+			</p>},
+	image_url: 'http://beachmuser.com/wp/wp-content/uploads/2014/10/IMG_3231.jpg',
+	price: 0.00,
+	availability_date: '2014-11-18',
+	category: 'free',
+	active: true )
+
