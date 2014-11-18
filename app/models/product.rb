15d39248ad60664667581099e08cbfb359023a02
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-18 16:29:58 +0000

    Add validation for image_url

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
 	validates :name, :description, :image_url, :price, :availability_date, :category, :active, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.00}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG, or PNG image'
+	}
 end
