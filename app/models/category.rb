cdc230898d27a03b503ad604539f993c85cbd59a
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-19 15:21:36 +0000

    Fix issue with attr_accessible

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+  attr_accessible :name, :description, :category_id
   has_many :products
   validates :name, :active, :visible_on_homepage, presence: true
   validates :name, uniqueness: true
