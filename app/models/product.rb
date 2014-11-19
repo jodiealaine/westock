1e56765c16a1cb0cda63938c59cf70b3f71a0829
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-19 16:42:34 +0000

    Link Categories and Products in views and model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :category
-	validates :name, :description, :image_url, :price, :availability_date, :category, :active, presence: true
+	validates :name, :description, :image_url, :price, :availability_date, :category_id, :active, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.00}
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
